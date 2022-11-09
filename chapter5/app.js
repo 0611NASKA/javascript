8c00020e668e6b4a1c21eee36ef9342b9feae823
0611NASKA <010611naoki@gmail.com>
2022-11-09 15:42:03 +0900

    5章学習完了

---
+++ chapter5/app.js
@@ -0,0 +1,9 @@
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'height': '100px',
+      'width': '200px',
+      'background-color': '#0000FF'
+    }).slideUp();
+  });
+});
